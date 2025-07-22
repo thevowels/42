0cf0a00a316bbaec6c55bc7cf94b5950c0460b6f
Aung Phyo Htet <aphyo-ht@z2t1c4.42bangkok.com>
2025-07-23 05:13:49 +0700

    header files

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 20:34:20 by aphyo-ht          #+#    #+#             */
+/*   Updated: 2025/07/23 05:05:58 by aphyo-ht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+// #include "ft_stock_str.h"
+
+int	get_length(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_strcpy(char *dest, char *str)
+{
+	while (*str)
+	{
+		*dest = *str;
+		dest++;
+		str++;
+	}
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	int			size;
+	char		*tmp;
+	t_stock_str	*ptr;
+
+	i = 0;
+	ptr = malloc((ac + 1) * sizeof(t_stock_str));
+	while (i < ac)
+	{
+		size = get_length(av[i]);
+		ptr[i].size = size;
+		ptr[i].str = av[i];
+		tmp = malloc(sizeof(char) * size);
+		ft_strcpy(tmp, av[i]);
+		ptr[i].copy = tmp;
+		i++;
+	}
+	ptr[i].str = 0;
+	return (ptr);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char				*strs[] = {"hello", "world", "this", "is", "a", "test"};
+	int					size;
+	struct s_stock_str	*result;
+	int					i;
+
+	size = sizeof(strs) / sizeof(char *);
+	result = ft_strs_to_tab(size, strs);
+	i = 0;
+	//ft_show_tab(result);
+	while(result[i].str)
+	{
+		printf("Pointer %p \n", result);
+		printf("str : %s\n", result[i].str);
+		printf("copy : %s\n", result[i].copy);
+		printf("str : %i\n", result[i].size);
+		printf("-------------------------\n");
+		i++;
+		printf("Index is now %i", i);
+	}
+	// Free the allocated memory
+	for (int i = 0; i < size; i++)
+	{
+		free(result[i].copy);
+	}
+	free(result);
+	return (0);
+}
+*/
---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aphyo-ht <aphyo-ht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 04:20:05 by aphyo-ht          #+#    #+#             */
+/*   Updated: 2025/07/23 05:04:26 by aphyo-ht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+// #include <stdlib.h>
+// #include "ft_stock_str.h"
+
+// struct s_stock_str *ft_strs_to_tab(int ac, char **av);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_putint(int i)
+{
+	char	c;
+
+	while (i >= 10)
+	{
+		c = '0' + i % 10;
+		write(1, &c, 1);
+		i = i / 10;
+	}
+	c = '0' + i % 10;
+	write(1, &c, 1);
+	write(1, "\n", 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		ft_putstr(par[i].str);
+		ft_putstr(par[i].copy);
+		ft_putint(par[i].size);
+		i++;
+	}
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char				*strs[] = {"hello", "world", "this", "is", "a", "test"};
+	int					size;
+	struct s_stock_str	*result;
+
+	size = sizeof(strs) / sizeof(char *);
+	result = ft_strs_to_tab(size, strs);
+	ft_show_tab(result);
+	// Free the allocated memory
+	for (int i = 0; i < size; i++)
+	{
+		free(result[i].copy);
+	}
+	free(result);
+	return (0);
+}
+*/
